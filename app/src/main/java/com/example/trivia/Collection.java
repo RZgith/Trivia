2d0c26cbdcf51638114bcddac1459fc61abf3cf0
User <roni.za111@gmail.com>
2025-02-17 16:46:47 +0200

    first version

---
+++ app/src/main/java/com/example/trivia/Collection.java
@@ -0,0 +1,48 @@
+package com.example.trivia;
+
+import java.util.ArrayList;
+
+public class Collection {
+    private ArrayList<Question> arr;
+    private int index;
+    public Collection()
+    {
+        arr=new ArrayList<>();
+
+        Question q1=new Question("1+10","11","3","100","110",1);
+        Question q2=new Question("1+2","12","3","100","30",2);
+        Question q3=new Question("1+4","15","3","5","11",3);
+        Question q4=new Question("3+4","7","9","34","1",1);
+        Question q5=new Question("10+6","15","106","16","11",3);
+        Question q6=new Question("5+6","12","1","66","11",4);
+        Question q7=new Question("10+3","13","16","1","11",1);
+        Question q8=new Question("4+6","15","10","16","11",2);
+
+
+        arr.add(q1);
+        arr.add(q2);
+        arr.add(q3);
+        arr.add(q4);
+        arr.add(q5);
+
+    }
+    public void initQuestion()
+    {
+        index=0;
+    }
+
+    public Question getNextQuestion()
+    {
+        Question q=arr.get(index);
+        index++;
+        return q;
+    }
+    public boolean isNotLastQuestion()
+    {
+        return (index<arr.size());
+    }
+    public int getIndex()
+    {
+        return index;
+    }
+}
---
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.trivia;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityResultLauncher<Intent> launcher;
+    private FbModule fbModule;
+    private String backgrounColor="";
+
+    private ConstraintLayout ll;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ll=findViewById(R.id.main);
+
+        fbModule=new FbModule(this);
+
+        launcher=registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult o) {
+
+                        if (o.getResultCode()==RESULT_OK)
+                        {
+                            Intent data=o.getData();
+                            String str=data.getStringExtra("color");
+                           // Toast.makeText(MainActivity.this, ""+str, Toast.LENGTH_SHORT).show();
+                            fbModule.writeBackgroundColorToFb(str);
+
+
+                        }
+
+                    }
+                }
+        );
+
+    }
+
+    public void onStartGame(View view) {
+        Intent intent=new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void onSetting(View view) {
+
+        Intent i=new Intent(this, SettingActivity.class);
+        launcher.launch(i);
+
+    }
+
+    public void onInstruction(View view) {
+    }
+
+    public void setNewColorFromFb(String str) {
+        this.backgrounColor=str;
+        //Toast.makeText(this, ""+backgrounColor, Toast.LENGTH_SHORT).show();
+        setBackgrounColor(str);
+
+    }
+
+    public void setBackgrounColor(String color)
+    {
+        switch (color)
+        {
+            case "Red":
+            {
+                ll.setBackgroundColor(Color.argb(255,240,128,128));
+                break;
+            }
+            case "Blue":
+            {
+                ll.setBackgroundColor(Color.argb(255,135,206,235));
+                break;
+            }
+            case "Pink":
+            {
+                ll.setBackgroundColor(Color.argb(255,219,112,147));
+                break;
+            }
+
+            case "Yellow":
+            {
+                ll.setBackgroundColor(Color.argb(255,255,250,205));
+                break;
+            }
+            default:
+                ll.setBackgroundColor(Color.WHITE);
+        }
+    }
+}
